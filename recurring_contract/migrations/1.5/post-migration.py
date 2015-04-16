d4f2bf0b4c2867c82b3006f4c41567e8caa480e8
David Coninckx <davconinckx@gmail.com>
2015-04-16 09:38:10 +0200

    Add migrations script for recurring contract

---
+++ recurring_contract/migrations/1.5/post-migration.py
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: Coninckx David <david@coninckx.com>
+#
+#    The licence is in the file __openerp__.py
+#
+##############################################################################
+import sys
+import pdb
+
+def migrate(cr, version):
+    reload(sys)
+    sys.setdefaultencoding('UTF8')
+
+    if not version:
+        return
+    delay_dict = {'annual': 12, 'biannual': 6, 'fourmonthly': 4,
+                      'quarterly': 3, 'bimonthly': 2, 'monthly':1}
+    cr.execute(
+        '''
+        SELECT id, advance_billing_moved0 FROM recurring_contract_group
+        '''
+    )
+
+    contract_groups = cr.fetchall()
+
+    for contract_group in contract_groups:
+        delay = delay_dict[contract_group[1]] or 1
+        cr.execute(
+            '''
+            UPDATE recurring_contract_group
+            SET recurring_value = {0}, advance_billing = {0}
+            WHERE id = {1}
+            '''.format(delay, contract_group[0])
+           
+        )
+         
---
+++ recurring_contract/migrations/1.5/pre-migration.py
@@ -0,0 +1,15 @@
+##############################################################################
+#
+#    Copyright (C) 2015 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: Coninckx David <david@coninckx.com>
+#
+#    The licence is in the file __openerp__.py
+#
+##############################################################################
+import sys
+import pdb
+
+def migrate(cr, version):
+    pass
+         
