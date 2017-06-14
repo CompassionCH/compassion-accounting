0c46531fd8eb4b492f4f1645a444eb3914dec3af
Emanuel Cino <ecino@compassion.ch>
2017-06-14 11:08:52 +0200

    Add migration script for recurring_contract

---
+++ recurring_contract/migrations/9.0.1.0.0/pre-migration.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2017 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: Emanuel Cino <ecino@compassion.ch>
+#
+#    The licence is in the file __openerp__.py
+#
+##############################################################################
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate(use_env=True)
+def migrate(env, version):
+    if not version:
+        return
+
+    cr = env.cr
+    # Install dependency
+    openupgrade.logged_query(cr, """
+        UPDATE ir_module_module
+        SET state='to install'
+        WHERE name = 'account_payment_mode' AND state='uninstalled';
+    """)
