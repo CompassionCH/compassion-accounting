33a75e334540da35b8040bdebebc993e342646c5
Emanuel Cino <ecino@compassion.ch>
2018-01-30 11:19:03 +0100

    CO-1399 [ADD] new module for creating invoice by reconciling bank statement

---
+++ account_reconcile_create_invoice/models/__init__.py
@@ -0,0 +1,14 @@
+﻿# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2014 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: Emanuel Cino <ecino@compassion.ch>
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+
+from . import bank_statement
+from . import bank_statement_line
+from . import statement_operation
---
+++ account_reconcile_create_invoice/models/statement_operation.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2014 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: Emanuel Cino <ecino@compassion.ch>
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+
+from odoo import api, models, fields
+
+
+class AccountOperationTemplate(models.Model):
+
+    _inherit = 'account.reconcile.model'
+
+    product_id = fields.Many2one('product.product', 'Product')
+
+    @api.onchange('product_id')
+    def onchange_product_id(self):
+        self.account_id = self.product_id.property_account_income_id
+
+    @api.model
+    def product_changed(self, product_id):
+        """
+        Helper to get the account and analytic account in reconcile view.
+        :param product_id:
+        :return: account_id, analytic_id
+        """
+        if product_id:
+            product = self.env['product.product'].browse(product_id)
+            account_id = product.property_account_income_id.id
+            return {
+                'account_id': account_id,
+            }
+        return False
