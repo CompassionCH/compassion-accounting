611c89ab989ea458687ca6ba3779ccdbfbde4e5b
Civiliste Compassion <gigon.quentin@gmail.com>
2018-08-21 15:25:40 +0200

    CO-2004 created new module to extend existing view and add an option to move all dratf/open bills dates 1 day after end of fiscal year

---
+++ account_move_fiscalyear/models/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2018 Compassion CH (http://www.compassion.ch)
+#    @author: Quentin Gigon <gigon.quentin@gmail.com>
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+
+from . import account_config_settings
+from . import res_company
