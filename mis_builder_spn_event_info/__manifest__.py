43c7c681248136e5d3ae21d9e199bd93b8a0fdbe
david wulliamoz <dwulliamoz@compassion.ch>
2019-03-22 14:14:02 +0100

    Add module MIS to report against Sponsorship & Events

---
+++ mis_builder_spn_event_info/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Compassion Suisse
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'MIS Builder event & sponsorship info',
+    'summary': """
+        Events, acquisition for MIS Builder""",
+    'version': '10.0.3.0.0',
+    'license': 'AGPL-3',
+    'author': 'compassion suisse',
+    'website': 'https://github.com/OCA/mis-builder',
+    'depends': [
+        'mis_builder',
+        'account',
+    ],
+    'data': [
+        'security/mis_spn_event_info.xml',
+        'views/mis_spn_event_info.xml',
+    ],
+    'installable': True,
+    'maintainers': ['davidwul'],
+}
