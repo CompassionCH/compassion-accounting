f655ab1a955d3eb27395414423e3225cc9f217d5
qgigon <gigon.quentin@gmail.com>
2020-01-14 14:49:59 +0100

    first steps done, almost installable. needs migration of sbc_compasion to continue

--- account_move_fiscalyear/__manifest__.py
+++ account_move_fiscalyear/__manifest__.py
@@ -10,14 +10,17 @@
 # pylint: disable=C8101
 {
     'name': 'Account move open bills to next fiscal year',
-    'version': '10.0.0.0.0',
+    'version': '11.0.0.0.0',
     'license': 'AGPL-3',
     'author': 'Compassion CH',
     'website': 'http://www.compassion.ch',
     'category': 'Accounting',
-    'depends': ['account'],
+    'depends': ['account',                      # source/addons/account
+                'child_compassion',
+                'account_lock_date_update'
+                ],
     'external_dependencies': {},
     'data': ['views/res_config_bills_view.xml'],
     'demo': [],
-    'installable': False,
+    'installable': True,
 }
