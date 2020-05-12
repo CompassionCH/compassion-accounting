8f9125f78b25d601bb50f258594be1f9188c8a87
Sylvain Losey <sylvainlosey@gmail.com>
2020-05-12 12:57:21 +0200

    CO-3138 Fix product_id field of reconcile js view
    - Overload the original _renderCreate function and add product_id field in the same manner as the other fields. It doesn't seem possible to call super and add the field afterwards.
    - Add 'product_id' to the quickCreateFields
    - Subclass ManualModel instead of StatementModel

--- account_reconcile_create_invoice/static/src/js/account_move_reconciliation.js
+++ account_reconcile_create_invoice/static/src/js/account_move_reconciliation.js
@@ -35,37 +35,116 @@ odoo.define('account_reconcile_create_invoice.reconciliation', function (require
     // Extend the class written in module account (bank statement view)
     reconciliation_renderer.LineRenderer.include({
 
-        // Add field product_id for invoice creation (this generates the field in the view)
-        _renderCreate: function (state) {
-            this._super(state);
-            var self = this;
-            this.model.makeRecord('account.bank.statement.line', [{
-                relation: 'product.product',
-                type: 'many2one',
-                name: 'product_id',
-            }], {
-                product_id: {string: _t("Product")}
-            }).then(function (recordID) {
-                self.handleCreateProductRecord = recordID;
-                var record = self.model.get(self.handleCreateProductRecord);
-                self.fields.product_id = new relational_fields.FieldMany2One(self,
-                    'product_id', record, {mode: 'edit'});
-                var $create = self.$('.create');
-                self.fields.product_id.appendTo($create.find('.create_product_id .o_td_field'));
-            });
-        },
+      // Overload of the default function in 'account.ReconciliationRenderer' which adds the product_id field. 
+      // It doesn't seem possible to call the superclass function and then add the field as basic_model.js 
+      // doesn't have a method to add fields to an existing record, only makeRecord.
+       _renderCreate: function (state) {
+          var self = this;
+          this.model.makeRecord('account.bank.statement.line', [{
+              relation: 'account.account',
+              type: 'many2one',
+              name: 'account_id',
+              domain: [['company_id', '=', state.st_line.company_id], ['deprecated', '=', false]],
+          }, {
+              relation: 'account.journal',
+              type: 'many2one',
+              name: 'journal_id',
+              domain: [['company_id', '=', state.st_line.company_id]],
+          }, {
+              relation: 'account.tax',
+              type: 'many2one',
+              name: 'tax_id',
+              domain: [['company_id', '=', state.st_line.company_id]],
+          }, {
+              relation: 'account.analytic.account',
+              type: 'many2one',
+              name: 'analytic_account_id',
+          }, {
+              relation: 'account.analytic.tag',
+              type: 'many2many',
+              name: 'analytic_tag_ids',
+          }, {
+              type: 'boolean',
+              name: 'force_tax_included',
+          }, {
+              type: 'char',
+              name: 'label',
+          }, {
+              type: 'float',
+              name: 'amount',
+          }, {
+              type: 'char', //TODO is it a bug or a feature when type date exists ?
+              name: 'date',
+          }, {
+              relation: 'product.product',
+              type: 'many2one',
+              name: 'product_id',
+          }], {
+              account_id: {
+                  string: _t("Account"),
+              },
+              label: { string: _t("Label") },
+              amount: { string: _t("Account") },
+              product_id: { string: _t("Product") }
+          }).then(function (recordID) {
+              self.handleCreateRecord = recordID;
+              var record = self.model.get(self.handleCreateRecord);
 
-        update: function (state) {
-            this._super(state);
-            // TODO attempt to render product_id value into view not working
-            if (state.createForm) {
-                var data = this.model.get(this.handleCreateProductRecord).data;
-                this.model.notifyChanges(this.handleCreateRecord, state.createForm);
-            }
-        }
+              self.fields.account_id = new relational_fields.FieldMany2One(self,
+                  'account_id', record, { mode: 'edit' });
+
+              self.fields.journal_id = new relational_fields.FieldMany2One(self,
+                  'journal_id', record, { mode: 'edit' });
+
+              self.fields.tax_id = new relational_fields.FieldMany2One(self,
+                  'tax_id', record, { mode: 'edit', additionalContext: { append_type_to_tax_name: true } });
+
+              self.fields.analytic_account_id = new relational_fields.FieldMany2One(self,
+                  'analytic_account_id', record, { mode: 'edit' });
+
+              self.fields.analytic_tag_ids = new relational_fields.FieldMany2ManyTags(self,
+                  'analytic_tag_ids', record, { mode: 'edit' });
+
+              self.fields.force_tax_included = new basic_fields.FieldBoolean(self,
+                  'force_tax_included', record, { mode: 'edit' });
+
+              self.fields.label = new basic_fields.FieldChar(self,
+                  'label', record, { mode: 'edit' });
+
+              self.fields.amount = new basic_fields.FieldFloat(self,
+                  'amount', record, { mode: 'edit' });
+
+              self.fields.date = new basic_fields.FieldDate(self,
+                  'date', record, { mode: 'edit' });
+
+              self.fields.product_id = new relational_fields.FieldMany2One(self,
+                  'product_id', record, { mode: 'edit' });
+
+              var $create = $(qweb.render("reconciliation.line.create", { 'state': state, 'group_tags': self.group_tags, 'group_acc': self.group_acc }));
+              self.fields.account_id.appendTo($create.find('.create_account_id .o_td_field'))
+                  .then(addRequiredStyle.bind(self, self.fields.account_id));
+              self.fields.journal_id.appendTo($create.find('.create_journal_id .o_td_field'));
+              self.fields.tax_id.appendTo($create.find('.create_tax_id .o_td_field'));
+              self.fields.analytic_account_id.appendTo($create.find('.create_analytic_account_id .o_td_field'));
+              self.fields.analytic_tag_ids.appendTo($create.find('.create_analytic_tag_ids .o_td_field'));
+              self.fields.force_tax_included.appendTo($create.find('.create_force_tax_included .o_td_field'))
+              self.fields.label.appendTo($create.find('.create_label .o_td_field'))
+                  .then(addRequiredStyle.bind(self, self.fields.label));
+              self.fields.amount.appendTo($create.find('.create_amount .o_td_field'))
+                  .then(addRequiredStyle.bind(self, self.fields.amount));
+              self.fields.date.appendTo($create.find('.create_date .o_td_field'));
+              self.fields.product_id.appendTo($create.find('.create_product_id .o_td_field'));
+              self.$('.create').append($create);
+
+              function addRequiredStyle(widget) {
+                  widget.$el.addClass('o_required_modifier');
+              }
+          });
+      },
     });
 
-    reconciliation_model.StatementModel.include({
+    reconciliation_model.ManualModel.include({
+        quickCreateFields: ['product_id', 'account_id', 'amount', 'amount_type', 'analytic_account_id', 'journal_id', 'label', 'force_tax_included', 'tax_id', 'analytic_tag_ids'],
 
         updateProposition: function (handle, values) {
             // Update other fields when product_id is changed
@@ -92,9 +171,6 @@ odoo.define('account_reconcile_create_invoice.reconciliation', function (require
 
         quickCreateProposition: function (handle, reconcileModelId) {
             // Add product field from the reconcile model into the proposition
-            // TODO the value is correctly set into the JS variables but the field value
-            // does not show the value set. The field in the view seems not linked to the
-            // underlying data
             this._super(handle, reconcileModelId);
             var line = this.getLine(handle);
             var reconcileModel = _.find(this.reconcileModels, function (r) {return r.id === reconcileModelId;});
@@ -104,7 +180,6 @@ odoo.define('account_reconcile_create_invoice.reconciliation', function (require
             }
             return this._computeLine(line);
         }
-
     });
 
     return {
