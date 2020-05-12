9f61c3f636aca78d900ec949d20c7a5853ed5e0a
qgigon <gigon.quentin@gmail.com>
2020-05-12 16:05:34 +0200

    fix flake

--- l10n_ch_import_camt054/models/account_bank_statement_line.py
+++ l10n_ch_import_camt054/models/account_bank_statement_line.py
@@ -13,7 +13,7 @@ class AccountBankStatementLine(models.Model):
     def process_reconciliation(self, counterpart_aml_dicts=None,
                                payment_aml_rec=None, new_aml_dicts=None):
         counterpart_moves = super().process_reconciliation(
-                counterpart_aml_dicts, payment_aml_rec, new_aml_dicts)
+            counterpart_aml_dicts, payment_aml_rec, new_aml_dicts)
 
         if hasattr(self, 'acct_svcr_ref') and self.acct_svcr_ref:
             for move_line in counterpart_moves.line_ids:
