c87f916c387fc834ea4e38249dbac7ca6fd47fe0
William Martin <wmartin@compassion.ch>
2020-06-08 16:27:38 +0200

    CO-3246 fixed code

---
+++ account_analytic_compassion/models/__init__.py
@@ -0,0 +1,10 @@
+##############################################################################
+#
+#    Copyright (C) 2020 Compassion CH (http://www.compassion.ch)
+#    @author: William Martin <wmartin@compassion.ch>
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+
+from . import product
