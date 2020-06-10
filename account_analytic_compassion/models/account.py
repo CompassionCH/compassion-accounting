4bb55cfbef43f9690baaf6973d4c98d8313c022f
William Martin <wmartin@compassion.ch>
2020-06-10 14:47:55 +0200

    CO-3249 fix exclusive constraint

---
+++ account_analytic_compassion/models/account.py
@@ -0,0 +1,21 @@
+##############################################################################
+#
+#    Copyright (C) 2020 Compassion CH (http://www.compassion.ch)
+#    @author: William Martin <wmartin@compassion.ch>
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+
+from odoo import _, api, models
+import re
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    @api.multi
+    def _check_analytic_distribution_required_msg(self):
+        message = super(AccountMoveLine, self)._check_analytic_distribution_required_msg()
+        pattern = r'.*mutually exclusive.*'
+        if message and not re.match(pattern, message, re.IGNORECASE):
+            return message
