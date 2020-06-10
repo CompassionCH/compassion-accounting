6cabf5fff5aaa294c0dfba75793d9cd5f77d0d76
William Martin <wmartin@compassion.ch>
2020-06-10 15:00:07 +0200

    CO-3249 fix code

--- account_analytic_compassion/models/account.py
+++ account_analytic_compassion/models/account.py
@@ -7,7 +7,7 @@
 #
 ##############################################################################
 
-from odoo import _, api, models
+from odoo import api, models
 import re
 
 
@@ -16,7 +16,8 @@ class AccountMoveLine(models.Model):
 
     @api.multi
     def _check_analytic_distribution_required_msg(self):
-        message = super(AccountMoveLine, self)._check_analytic_distribution_required_msg()
+        message = super(AccountMoveLine, self).\
+            _check_analytic_distribution_required_msg()
         pattern = r'.*mutually exclusive.*'
         if message and not re.match(pattern, message, re.IGNORECASE):
             return message
