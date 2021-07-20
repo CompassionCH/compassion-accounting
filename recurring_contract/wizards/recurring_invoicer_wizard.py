b2253e413a4b007010a5747cef10df43d33ddb97
Jonathan Guerne <jonathan.guerne@gmail.com>
2021-07-20 14:05:48 +0200

    CO-3694 generate invoice based on advanced billing month and not always for one month in advance (#130)
    * CO-3694 generate invoice based on advanced billing month and not always for one month in advance
    
    * CO-3694 use an sql query instead

--- recurring_contract/wizards/recurring_invoicer_wizard.py
+++ recurring_contract/wizards/recurring_invoicer_wizard.py
@@ -9,12 +9,8 @@
 ##############################################################################
 
 from odoo import fields, models, api
-import datetime
-from dateutil.relativedelta import relativedelta
-
 
 class InvoicerWizard(models.TransientModel):
-
     ''' This wizard generate invoices from contract groups when launched.
     By default, all contract groups are used.
     '''
@@ -25,12 +21,17 @@ class InvoicerWizard(models.TransientModel):
 
     @api.multi
     def generate(self):
-        date_limit = datetime.date.today() + relativedelta(months=+1)
 
         recurring_invoicer_obj = self.env['recurring.invoicer']
-        contract_groups = self.env['recurring.contract.group'].search([
-            ('next_invoice_date', '<=', date_limit),
-            ('next_invoice_date', '!=', False)])
+
+        self.env.cr.execute("""
+        SELECT id FROM recurring_contract_group
+        WHERE
+            next_invoice_date IS NOT NULL AND
+            next_invoice_date <= now() + interval '1 month' * advance_billing_months;
+        """)
+        gids = self.env.cr.fetchall()
+        contract_groups = self.browse(gids)
 
         invoicer = recurring_invoicer_obj.create({})
         # Add a job for all groups and start the job when all jobs are created.
