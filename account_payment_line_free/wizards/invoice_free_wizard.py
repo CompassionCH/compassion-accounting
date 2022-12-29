bbacb93d5bb52678de51c9de059ed684f57fb2d7
Simon Gonzalez <simon.gonzalez@bluewin.ch>
2022-12-29 11:29:39 +0100

    CP-316 add action on payment order for invoices to be unlinked.

--- account_payment_line_free/wizards/invoice_free_wizard.py
+++ account_payment_line_free/wizards/invoice_free_wizard.py
@@ -3,6 +3,7 @@
 # @author: david wulliamoz, Emanuel Cino
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import models, api
+from odoo.exceptions import UserError
 
 
 class AccountInvoiceFree(models.TransientModel):
@@ -17,3 +18,15 @@ class AccountInvoiceFree(models.TransientModel):
         inv_obj = self.env['account.move']
         invoices = inv_obj.browse(self.env.context.get('active_ids'))
         return invoices.free_payment_lines()
+
+    def invoice_free_from_payment_order(self):
+        # Retrieve all payment line that hasn't been paid
+        inv_list = []
+        payment_orders = self.env["account.payment.order"].browse(self.env.context.get("active_ids"))
+        for pay_ord in payment_orders:
+            for line in pay_ord.payment_line_ids:
+                if line.move_line_id.payment_state != 'paid'\
+                        and line.move_line_id:
+                    inv_list.append(line.move_line_id.move_id.id)
+        # Unlink the invoices and the payment order
+        return self.with_context({"active_ids": inv_list}).invoice_free()
