d3759e800cbf428ee56fe9158992a4ac605d9304
Simon Gonzalez <simon.gonzalez@bluewin.ch>
2023-01-06 11:45:12 +0100

    CP-239 Correction of the cancel invoices

--- recurring_contract/models/recurring_contract.py
+++ recurring_contract/models/recurring_contract.py
@@ -145,6 +145,7 @@ class RecurringContract(models.Model):
             day_l.append((str(curr_day), str(curr_day)))
             curr_day += 1
         return day_l
+
     @api.depends('contract_line_ids', 'contract_line_ids.amount',
                  'contract_line_ids.quantity')
     def _compute_total_amount(self):
@@ -586,18 +587,20 @@ class RecurringContract(models.Model):
             paid_invoices.reconcile_after_clean()
 
             # Cancel open or draft invoices
-            invoices = self.env['account.move.line'].search([('contract_id', 'in', self.ids),
-                                                             ('state', 'not in', ('paid', 'cancel')),
-                                                             ('due_date', '>=', since_date)]).mapped('move_id')
+            invoices_lines = self.env['account.move.line'].search([('contract_id', 'in', self.ids),
+                                                                   ('state', 'not in', ('paid', 'cancel')),
+                                                                   ('due_date', '>=', since_date)])
 
             # Multi contracts invoices should delete just their lines
-            empty_invoices = self.env['account.invoice']
-            to_remove_invl = self.env['account.invoice.line']
-            for inv_line in invoices:
+            empty_invoices = self.env['account.move']
+            to_remove_invl = self.env['account.move.line']
+            invoices = invoices_lines.mapped("move_id")
+            for inv_line in invoices_lines:
                 invoice = inv_line.move_id
                 # Check if invoice is empty after removing the invoice_lines
                 # of the given contract
-                remaining_lines = invoice.invoice_line_ids.filtered(lambda l: not l.contract_id or l.contract_id not in self)
+                remaining_lines = invoice.invoice_line_ids.filtered(
+                    lambda l: not l.contract_id or l.contract_id not in self)
                 if remaining_lines:
                     # We can move or remove the line
                     to_remove_invl |= inv_line
