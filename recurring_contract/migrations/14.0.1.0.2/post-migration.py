a76e7ba4a8a2f2e4a428f85ffb2f533cfed0ca94
Simon Gonzalez <45272875+simgonzalez@users.noreply.github.com>
2023-02-27 12:49:51 +0100

    CP-388 dont change past invoices (#182)

--- recurring_contract/migrations/14.0.1.0.2/post-migration.py
+++ recurring_contract/migrations/14.0.1.0.2/post-migration.py
@@ -7,3 +7,9 @@ def migrate(cr, version):
             "from recurring_contract_group c "
             "inner join recurring_contract on group_id = c.id"
         )
+        cr.execute(
+
+        """update recurring_contract_group
+        set invoice_suspended_until = (select next_invoice_date from recurring_contract where group_id = recurring_contract_group.id)
+        where id in (select group_id from recurring_contract rc where state = 'active' group by group_id having count( *) = 1)"""
+        )
