cfaa1d931d9475ad2ad17c48a4bbbbe17876fdd6
Emanuel Cino <ecino@compassion.ch>
2023-03-06 15:57:16 +0100

    CP-239 FIX migration query

--- recurring_contract/migrations/14.0.1.0.2/post-migration.py
+++ recurring_contract/migrations/14.0.1.0.2/post-migration.py
@@ -7,9 +7,11 @@ def migrate(cr, version):
             "from recurring_contract_group c "
             "inner join recurring_contract on group_id = c.id"
         )
-        cr.execute(
-
-        """update recurring_contract_group
-        set invoice_suspended_until = (select next_invoice_date from recurring_contract where group_id = recurring_contract_group.id)
-        where id in (select group_id from recurring_contract rc where state = 'active' group by group_id having count( *) = 1)"""
-        )
+        cr.execute("""
+        update recurring_contract_group
+        set invoice_suspended_until = (
+            select min(next_invoice_date) from recurring_contract
+            where group_id = recurring_contract_group.id and state = 'active'
+            group by group_id
+            having count(*) = 1)
+        """)
