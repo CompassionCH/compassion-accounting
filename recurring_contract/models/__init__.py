779822bb7db078859ba2b4056fa6a82a35825393
Simon <simon.gonzalez@bluewin.ch>
2023-05-26 09:44:57 +0200

    T0095 add survival sponsorship mecanism * Add a sponsorship type * Add some specific fields on the product that are related to this new sponsorship type * Add a base automation that warn admin users in case we reach the maximum number of slots available in a project.

--- recurring_contract/models/__init__.py
+++ recurring_contract/models/__init__.py
@@ -5,6 +5,8 @@ from . import recurring_invoicer
 from . import product_pricelist_item
 from . import recurring_contract
 from . import recurring_contract_line
+from . import product_product
 from . import res_config_settings
 from . import utm
 from . import end_reason
+from . import move_line
