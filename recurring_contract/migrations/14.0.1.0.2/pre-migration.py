9c9cc52050b3bda9ab87ef97558501bb916edd55
Emanuel Cino <ecino@compassion.ch>
2024-01-22 15:35:40 +0100

    T0718 FIX recurring_contract migration
    - precompute fields to avoid memory problems

--- recurring_contract/migrations/14.0.1.0.2/pre-migration.py
+++ recurring_contract/migrations/14.0.1.0.2/pre-migration.py
@@ -7,6 +7,9 @@ def migrate(env, version):
     openupgrade.add_fields(env, [
         ('last_payment', 'account.move', 'account_move', 'date', False, 'recurring_contract', False),
         ('missing_invoices', 'recurring.contract', 'recurring_contract', 'boolean', False, 'recurring_contract', False),
+        ('due_invoice_ids', 'recurring.contract', 'recurring_contract', 'many2many', False, 'recurring_contract', False),
+        ('amount_due', 'recurring.contract', 'recurring_contract', 'integer', False, 'recurring_contract', False),
+        ('months_due', 'recurring.contract', 'recurring_contract', 'integer', False, 'recurring_contract', False),
     ])
     openupgrade.logged_query(env.cr,
         """
@@ -27,3 +30,10 @@ def migrate(env, version):
         WHERE payment_state = 'paid' AND move_type IN ('out_invoice', 'in_invoice');
         """
     )
+    openupgrade.logged_query(env.cr,
+"""INSERT INTO recurring_contract_due_invoice_rel (recurring_contract_id, account_move_id)
+            SELECT DISTINCT aml.contract_id, aml.move_id
+            FROM account_move_line aml
+            JOIN account_move m ON m.id = aml.move_id
+            WHERE aml.contract_id IS NOT NULL
+            AND m.payment_state != 'paid' AND m.state = 'posted' AND aml.due_date < NOW()""")
