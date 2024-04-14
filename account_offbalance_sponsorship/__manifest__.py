e668bdf35d1e40c43447b92328353274e8321df1
David Wulliamoz <dwulliamoz@compassion.ch>
2024-04-14 14:12:37 +0200

    off balance spn module

---
+++ account_offbalance_sponsorship/__manifest__.py
@@ -0,0 +1,69 @@
+##############################################################################
+#
+#       ______ Releasing children from poverty      _
+#      / ____/___  ____ ___  ____  ____ ___________(_)___  ____
+#     / /   / __ \/ __ `__ \/ __ \/ __ `/ ___/ ___/ / __ \/ __ \
+#    / /___/ /_/ / / / / / / /_/ / /_/ (__  |__  ) / /_/ / / / /
+#    \____/\____/_/ /_/ /_/ .___/\__,_/____/____/_/\____/_/ /_/
+#                        /_/
+#                            in Jesus' name
+#
+#    Copyright (C) 2014-today Compassion CH (http://www.compassion.ch)
+#    @author: David Wulliamoz <dwulliamoz@compassion.ch>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': "account_offbalance_sponsorship",
+
+    'summary': """
+        Off-Balance accounting for sponsorships.
+""",
+
+    'description': """
+        It's not possible to use the stock off-balance feature because
+        it's not possible to have a reconciliable off-balance account. So we use the 9xxx account and a configuration to
+        define:
+        - receivable (off-balance): A
+        - asset (off-balance): B
+        this function adds in the payment move if there is an off-balance (A) receivable account:
+        - the off-balance asset (B)
+        - add the outstanding account again.    """,
+
+    'author': "Compassion CH",
+    'website': "http://www.compassion.ch",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'sponsorship and donation',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base',
+                'sponsorship_compassion',
+                ],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/res_config_view.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ account_offbalance_sponsorship/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from . import bank_statement_line
+from . import res_config
