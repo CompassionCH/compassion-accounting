55fb5812ca8c6e9672e5bc957ba6c935a29a87a2
Emanuel Cino <ecino@compassion.ch>
2024-08-27 15:23:25 +0200

    [MIG] recurring_contract/: Migration to 16.0

--- recurring_contract/migrations/14.0.1.0.4/pre-migration.py
+++
@@ -1,6 +0,0 @@
-def migrate(cr, version):
-    if version:
-        cr.execute(
-            "update account_move set invoice_date = date, invoice_date_due = date "
-            "where invoice_date is null"
-        )
