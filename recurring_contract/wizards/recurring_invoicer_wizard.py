87d0a5885cfece22f516ce369950459d7903cbb8
Nicolas Praz <praznicolas@gmail.com>
2024-08-30 15:50:56 +0200

    added comment

--- recurring_contract/wizards/recurring_invoicer_wizard.py
+++ recurring_contract/wizards/recurring_invoicer_wizard.py
@@ -43,8 +43,8 @@ class InvoicerWizard(models.TransientModel):
         group_ids = [r[0] for r in self.env.cr.fetchall()]
         groups = self.env["recurring.contract.group"].browse(group_ids)
 
+        # TOTO : REVERT DUMMY CHANGES BEFORE PUBLISHING PR
         #groups = self.env["recurring.contract.group"].browse(20772)
-
         groups = groups[1]
 
         # Add a job for all groups and start the job when all jobs are created.
