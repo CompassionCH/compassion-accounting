037a0d6c2c64e17567ba1388983dcf5c01e1c66b
Emanuel Cino <ecino@compassion.ch>
2024-10-01 09:01:09 +0200

    T1862 FIX payment_mode removed after contract ending
    - Because payment_mode is computed, it's recomputed when invoices are modified and not taken from the contract. With this workaround we ensure it's not changed when an invoice is modified upon contract ending.

--- recurring_contract/models/recurring_contract.py
+++ recurring_contract/models/recurring_contract.py
@@ -593,7 +593,10 @@ class RecurringContract(models.Model):
             )
             if remaining_lines:
                 # We can move or remove the line
-                invoice.write({"invoice_line_ids": [(2, inv_line.id)]})
+                invoice.write({
+                    "invoice_line_ids": [(2, inv_line.id)],
+                    "payment_mode_id": invoice.payment_mode_id.id,
+                })
             else:
                 # The invoice would be empty if we remove the line
                 empty_invoices |= invoice
