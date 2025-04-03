8fb80d10953ab593a2264dee8699fef0e7119251
Emanuel Cino <ecino@compassion.ch>
2025-04-03 15:57:17 +0200

    T2337 FIX code

--- account_reconcile_compassion_ee/models/account_reconcile_model.py
+++ account_reconcile_compassion_ee/models/account_reconcile_model.py
@@ -43,10 +43,10 @@ class AccountReconcileModel(models.Model):
         elif self.only_this_month:
             date_start = st_line_date.replace(day=1)
             date_end = date_start.replace(month=date_start.month + 1)
-            domain.append(
+            domain.extend([
                 ("date", ">=", fields.Date.to_string(date_start)),
                 ("date", "<", fields.Date.to_string(date_end)),
-            )
+            ])
         if self.matching_account_id:
             domain.append(("account_id", "=", self.matching_account_id.id))
         return domain
