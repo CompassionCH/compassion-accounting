fb640223f885e5f6d050717bf236c3b0e99b77a5
Emanuel Cino <ecino@compassion.ch>
2025-10-09 13:51:33 +0200

    FIX unreconcile

--- account_offbalance_sponsorship/models/account_move.py
+++ account_offbalance_sponsorship/models/account_move.py
@@ -32,11 +32,35 @@ class AccountMove(models.Model):
                 "is_off_balance_generated"
             )
         if move_lines_to_unlink:
-            move_lines_to_unlink.with_context(force_delete=True).unlink()
+            # Check if moves will still be balanced after unlink
+            moves_dict = {
+                move: move_lines_to_unlink.filtered(
+                    lambda line, m=move: line.move_id == m
+                )
+                for move in move_lines_to_unlink.mapped("move_id")
+            }
+            for move in moves_dict.keys():
+                remaining_lines = move.line_ids - move_lines_to_unlink.filtered(
+                    lambda line, m=move: line.move_id == m
+                )
+                total_debit = sum(remaining_lines.mapped("debit"))
+                total_credit = sum(remaining_lines.mapped("credit"))
+                if not move.currency_id.is_zero(total_debit - total_credit):
+                    move_lines_to_unlink -= moves_dict[move]
+
+        if move_lines_to_unlink:
+            move_lines_to_unlink.remove_move_reconcile()
+            move_lines_to_unlink.with_context(
+                dynamic_unlink=True, force_delete=True
+            ).unlink()
 
         # Undo reconciliation for the statement lines
         st_lines = self.statement_line_ids
         for line in st_lines:
             line.action_undo_reconciliation()
+
+        partial = self.env["account.partial.reconcile"].browse(partial_id)
+        if not partial.exists():
+            return True
 
         return super().js_remove_outstanding_partial(partial_id)
