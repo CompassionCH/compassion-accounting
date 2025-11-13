27f6ef55129dda7fed91b6be628c8ae7da216d92
Emanuel Cino <ecino@compassion.ch>
2025-11-13 12:51:46 +0100

    [MIG] recurring_contract : import from 17.0

--- recurring_contract/__manifest__.py
+++ recurring_contract/__manifest__.py
@@ -63,7 +63,7 @@
         "data/queue_job.xml",
         "security/ir.model.access.csv",
     ],
-    'installable': False,
+    "installable": True,
     "assets": {
         "web.assets_backend": [
             "recurring_contract/static/src/components/account_payment.xml",
