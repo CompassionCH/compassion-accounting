ed0a2f13977852ba7ac29315483aba3c40f44139
Emanuel Cino <ecino@compassion.ch>
2026-01-15 13:26:44 +0100

    T2711 [MIG] import from reconcile_ee
    - Copy module for enterprise and remove components that are not existing (bank.reconcile.widget)

---
+++ account_reconcile_compassion_ce/models/account_reconcile_model.py
@@ -0,0 +1,70 @@
+from dateutil.relativedelta import relativedelta
+
+from odoo import api, fields, models
+
+
+class AccountReconcileModel(models.Model):
+    _inherit = "account.reconcile.model"
+
+    partner_matching_ids = fields.One2many(
+        "account.reconcile.model.partner.matching",
+        "model_id",
+        "Partner Matching Rules",
+        copy=True,
+    )
+    only_this_month = fields.Boolean(
+        default=False, help="Check to search only from the start of the month"
+    )
+    matching_account_id = fields.Many2one("account.account")
+
+    @api.onchange("past_months_limit")
+    def _uncheck_only_this_month(self):
+        if self.past_months_limit and self.only_this_month:
+            self.only_this_month = False
+
+    def _get_partner_from_mapping(self, st_line):
+        partner = super()._get_partner_from_mapping(st_line)
+        if not partner:
+            for matching in self.partner_matching_ids.filtered("is_active"):
+                partner = matching.match_line(st_line)
+                if partner:
+                    return partner
+        return partner
+
+    def _get_invoice_matching_amls_domain(self, st_line, partner):
+        domain = super()._get_invoice_matching_amls_domain(st_line, partner)
+        st_line_date = st_line.date or fields.Date.today()
+        if self.past_months_limit:
+            # Replace the date filter with the bs_date instead of today's date
+            date_limit = st_line_date - relativedelta(months=self.past_months_limit)
+            domain = [
+                ("date", ">=", fields.Date.to_string(date_limit))
+                if isinstance(item, tuple | list) and item and item[0] == "date"
+                else item
+                for item in domain
+            ]
+        elif self.only_this_month:
+            date_start = st_line_date.replace(day=1)
+            date_end = date_start + relativedelta(months=1)
+            domain.extend(
+                [
+                    ("date", ">=", fields.Date.to_string(date_start)),
+                    ("date", "<", fields.Date.to_string(date_end)),
+                ]
+            )
+        if self.matching_account_id:
+            domain.append(("account_id", "=", self.matching_account_id.id))
+        return domain
+
+    def _get_invoice_matching_amls_result(self, st_line, partner, candidate_vals):
+        result = super()._get_invoice_matching_amls_result(
+            st_line, partner, candidate_vals
+        )
+        # T2665 Nordic team would always like auto reconciliation to happen
+        if (
+            result is not None
+            and self.auto_reconcile
+            and self.rule_type == "invoice_matching"
+        ):
+            result["auto_reconcile"] = True
+        return result
