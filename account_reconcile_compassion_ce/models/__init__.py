ebde331546d5ff041e480d3c9d46c623703fc93e
Emanuel Cino <ecino@compassion.ch>
2026-01-15 15:15:56 +0100

    [MIG] T2711 account_reconcile_compassion_ce : migration to 18.0

--- account_reconcile_compassion_ce/models/__init__.py
+++ account_reconcile_compassion_ce/models/__init__.py
@@ -1,4 +1,3 @@
-from . import account_tax
 from . import account_reconcile_model
 from . import account_reconcile_model_partner_matching
 from . import bank_statement_line
--- account_reconcile_compassion_ce/models/bank_statement_line.py
+++ account_reconcile_compassion_ce/models/bank_statement_line.py
@@ -1,8 +1,6 @@
 import logging
-import time
 
-from odoo import _, fields, models
-from odoo.exceptions import UserError
+from odoo import api, fields, models
 
 _logger = logging.getLogger(__name__)
 
@@ -10,89 +8,55 @@ _logger = logging.getLogger(__name__)
 class BankStatementLine(models.Model):
     _inherit = "account.bank.statement.line"
 
-    def auto_reconcile_statement_lines(self, force_reconcile=False):
-        """Extracted from the auto_reconcile_statement_lines cron job to be called
-        independently and on selected lines."""
-        # The field `cron_last_check` will be written on all processed lines
-        # which requires them to be protected against
-        # concurrent update in order to avoid the whole transaction to be rollbacked.
-        _logger.info(
-            "auto_reconcile_statement_lines: acquiring row-level lock for %d lines: %s",
-            len(self),
-            self.ids,
-        )
-        self.env.cr.execute(
-            "SELECT 1 FROM account_bank_statement_line WHERE id in %s FOR UPDATE",
-            [tuple(self.ids)],
-        )
+    manual_product_id = fields.Many2one(
+        comodel_name="product.product",
+        check_company=True,
+        store=False,
+        default=False,
+        prefetch=False,
+    )
+    manual_contract_id = fields.Many2one(
+        comodel_name="recurring.contract",
+        check_company=True,
+        store=False,
+        default=False,
+        prefetch=False,
+    )
 
-        nb_auto_reconciled_lines = 0
-        start_time = fields.Datetime.now()
-        start_perf = time.perf_counter()
-        _logger.info("auto_reconcile_statement_lines: started at %s", start_time)
-
-        for st_line in self:
-            _logger.info(
-                "Processing bank statement line id=%s, name=%s",
-                st_line.id,
-                getattr(st_line, "name", "<no-name>"),
+    @api.onchange("manual_product_id")
+    def on_change_manual_product_id(self):
+        if self.manual_product_id:
+            self.manual_account_id = (
+                self.manual_product_id.property_account_income_id.id
             )
-            wizard = (
-                self.env["bank.rec.widget"]
-                .with_context(default_st_line_id=st_line.id)
-                .new({})
-            )
-            wizard._action_trigger_matching_rules()
-            _logger.info(
-                "After triggering matching rules: wizard.state=%s, "
-                "matching_rules_allow_auto_reconcile=%s",
-                getattr(wizard, "state", None),
-                getattr(wizard, "matching_rules_allow_auto_reconcile", None),
-            )
-            if (
-                wizard.state == "valid" and wizard.matching_rules_allow_auto_reconcile
-            ) or force_reconcile:
-                _logger.info(
-                    "Attempting auto-validate for statement line %s", st_line.id
-                )
-                try:
-                    wizard._action_validate()
-                    if st_line.is_reconciled:
-                        model_names = ", ".join(
-                            st_line.move_id.line_ids.reconcile_model_id.mapped("name")
-                        )
-                        _logger.info(
-                            "Line %s auto-reconciled using model(s): %s",
-                            st_line.id,
-                            model_names,
-                        )
-                        st_line.move_id.message_post(
-                            body=_(
-                                "This bank transaction has been automatically "
-                                "validated using the reconciliation model '%s'.",
-                                model_names,
-                            )
-                        )
-                        nb_auto_reconciled_lines += 1
-                    else:
-                        _logger.warning(
-                            "Validation ran but line %s is not reconciled", st_line.id
-                        )
-                except UserError as err:
-                    # Log why the validation failed for this line and continue
-                    _logger.warning(
-                        "UserError while auto-validating line %s: %s", st_line.id, err
-                    )
-                    continue
 
-        elapsed = time.perf_counter() - start_perf
-        _logger.info(
-            "auto_reconcile_statement_lines: finished. "
-            "auto-reconciled %d/%d lines in %.3fs",
-            nb_auto_reconciled_lines,
-            len(self),
-            elapsed,
+    @api.onchange("manual_contract_id")
+    def on_change_manual_contract_id(self):
+        # Make sure the reconcile data is updated when the contract changes
+        self._onchange_manual_reconcile_vals()
+
+    def _get_manual_reconcile_vals(self):
+        vals = super()._get_manual_reconcile_vals()
+        vals["product_id"] = (
+            self.manual_product_id.id,
+            self.manual_product_id.display_name,
         )
+        vals["contract_id"] = (
+            self.manual_contract_id.id,
+            self.manual_contract_id.display_name,
+        )
+        return vals
 
-        # write the last check timestamp
-        self.write({"cron_last_check": start_time})
+    def _reconcile_move_line_vals(self, line, move_id=False):
+        vals = super()._reconcile_move_line_vals(line, move_id=move_id)
+        vals["product_id"] = line["product_id"][0]
+        vals["contract_id"] = line["contract_id"][0]
+        return vals
+
+    def _check_line_changed(self, line):
+        res = super()._check_line_changed(line)
+        return (
+            res
+            or self.manual_product_id.id != line.get("product_id", (False,))[0]
+            or self.manual_contract_id.id != line.get("contract_id", (False,))[0]
+        )
